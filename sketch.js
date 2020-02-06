4a46890c39a73b930338b72b2453c7b517dfe0f2
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2020-02-06 17:35:03 +0100

    prova2

---
+++ sketch.js
@@ -0,0 +1,94 @@
+var initialInput;
+var submitButton;
+var database;
+
+var storage;
+var canvas;
+var capture;
+
+function preload(){
+  // put preload code here
+}
+
+function setup() {
+  //camera
+    canvas = createCanvas(150,100);
+    canvas.id('canvas');
+  capture = createCapture(VIDEO);
+  capture.size(700,400);
+  capture.id('capture');
+  // capture.hide();
+
+  //scores
+  initialInput = createInput('initials');
+  submitButton = createButton('submit');
+  submitButton.mousePressed(submitScore);
+
+  // convertButton = createButton('convert');
+  // convertButton.mousePressed(convertCanvasToImage);
+
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDHjHCgXbZpwH__Ht2cb-VblVuOdozk09Y",
+    authDomain: "photo-mosaic-prova-img.firebaseapp.com",
+    databaseURL: "https://photo-mosaic-prova-img.firebaseio.com",
+    projectId: "photo-mosaic-prova-img",
+    storageBucket: "photo-mosaic-prova-img.appspot.com",
+    messagingSenderId: "390227896604",
+    appId: "1:390227896604:web:3f87555db4a6b5d52ff2d9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  database = firebase.database();
+
+  var storageref = database.ref('photos');
+  storageref.on('value', errData);
+
+//------------------------------------------------
+
+}
+
+function draw() {
+
+  // put drawing code here
+}
+
+
+
+function submitScore(){
+  var pic = image(capture, 0, 0,150, 100);
+
+// console.log(data);
+
+var canvas = document.getElementById('canvas');
+var dataURL = canvas.toDataURL('image/png');
+// console.log(dataURL);
+
+// var new_img = createImg(dataURL);
+
+var data = {
+initials: initialInput.value(),
+photo_img: dataURL
+}
+
+var storageref = database.ref('photos');
+storageref.push(data);
+
+console.log(data);
+
+}
+
+
+
+function errData(err){
+  console.log('Error');
+  console.log(err)
+}
+
+// function convertCanvasToImage(canvas) {
+// 	var image = new Image();
+// 	image.src = canvas.toDataURL("image/png");
+// 	return image;
+// }
---
+++ sketch2.js
@@ -0,0 +1,83 @@
+var initialInput;
+var submitButton;
+var database;
+
+var storage;
+var canvas;
+var capture;
+
+function preload(){
+  // put preload code here
+}
+
+function setup() {
+  //camera
+  // capture.hide();
+
+  //scores
+
+  // convertButton = createButton('convert');
+  // convertButton.mousePressed(convertCanvasToImage);
+
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDHjHCgXbZpwH__Ht2cb-VblVuOdozk09Y",
+    authDomain: "photo-mosaic-prova-img.firebaseapp.com",
+    databaseURL: "https://photo-mosaic-prova-img.firebaseio.com",
+    projectId: "photo-mosaic-prova-img",
+    storageBucket: "photo-mosaic-prova-img.appspot.com",
+    messagingSenderId: "390227896604",
+    appId: "1:390227896604:web:3f87555db4a6b5d52ff2d9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  database = firebase.database();
+
+  var storageref = database.ref('photos');
+  storageref.once('value', gotData, errData);
+
+//------------------------------------------------
+
+}
+
+function draw() {
+
+  // put drawing code here
+}
+
+
+
+
+function gotData(data) {
+
+  var photolistings = selectAll('photolisting');
+  for (let i = 0; i < photolistings.length; i++){
+    photolistings[i].remove();
+  }
+
+var photos = data.val();
+var keys = Object.keys(photos);
+console.log(keys);
+for (let i = 0; i < keys.length; i++){
+  let k = keys[i];
+  let initials = photos[k].initials;
+  let photo_img = photos[k].photo_img;
+
+  var li = createElement('p', initials);
+  li.class('photolisting');
+  li.parent('photolist');
+
+  var li_img = createImg(photo_img);
+  li_img.class('photolisting');
+  li_img.parent('photolist');
+  }
+
+  // console.log(photo_img);
+}
+
+function errData(err){
+  console.log('Error');
+  console.log(err)
+}
